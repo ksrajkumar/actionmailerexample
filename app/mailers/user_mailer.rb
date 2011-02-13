0b75313b2aae3af0fb3adc3351befcea7cfe8ca3
ksrajkumar <ksrajkumar87@gmail.com>
2011-02-14 00:34:20 +0530

    actionmailer example

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ActionMailer::Base  
+  default :from => "ksrajkumar87@gmail.com"  
+  
+  def registration_confirmation(user)  
+	@user = user
+    mail(:to => user.email, :subject => "Registered")  
+  end  
+end  
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailit::Application.config.secret_token = '3fa97283908bafdf62fa4d2c1a6430a8ff621bfb4c46ec95ccd039c48026776aa12160618045125ff9ff9b2183d7ced00549518425403535d5194c7492ba8cf2'
